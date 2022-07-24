4d5e9900da7a81480041aabb9585603ba054c052
P.J. Finlay <pjappdevelopment@gmail.com>
2022-07-24 14:09:47 +0000

    Improve data generate script

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -1,31 +1,36 @@
+#!/usr/bin/env python3
+
 import argparse
 import random
+import pathlib
 
-parser = argparse.ArgumentParser(description="Generates summarization data")
+parser = argparse.ArgumentParser(description="Generate autoregressive data")
 parser.add_argument(
     "datafile",
     help="A file with text data",
 )
 args = parser.parse_args()
 
+source_path = pathlib.Path("source")
+target_path = pathlib.Path("target")
+
+MAX_LINE_LEN = 50
+
+if source_path.exists() or target_path.exists():
+    print("Source or target already exists")
+    exit(1)
+
+source_file = open(source_path, "a")
+target_file = open(target_path, "a")
+
 with open(args.datafile, 'r') as datafile:
-    data = datafile.read()
-    paragraphs = data.split("\n")
-    sourcelines = list()
-    targetlines = list()
-
-    for paragraph in paragraphs:
-        paragraph = paragraph[:300]
-        source_end_index = int(random.random() * (len(paragraph) - 1))
-        source = paragraph[:source_end_index]
-        target = paragraph[source_end_index:]
-        target_space_index = target.find(" ")
-        if target_space_index > 0:
-            target = target[:target_space_index]
-        sourcelines.append(source + "\n")
-        targetlines.append(target + "\n")
-
-    with open('output_data.src', "w") as sourcefile:
-        sourcefile.writelines(sourcelines)
-    with open('output_data.tgt', "w") as targetfile:
-        targetfile.writelines(targetlines)
+    for line in datafile:
+        line = line[:MAX_LINE_LEN]
+        source_end_index = int(random.random() * (len(line) - 1))
+        source_line = line[:source_end_index]
+        target_line = line[source_end_index:]
+        source_file.write(source_line + "\n")
+        target_file.write(target_line + "\n")
+        
+source_file.close()
+target_file.close()
